eaba3882f967f09769c32f9ba6d4371355770ac5
sachow12 <salomaovasques2@gmail.com>
2025-12-02 15:13:51 -0300

    refactor: improve code readability with comments in server.py

--- server.py
+++ server.py
@@ -16,20 +16,25 @@ print(f"Server listening on port {PORT}")
 while True:
     conn, addr = server_socket.accept()
     print(f"Connection from {addr}")
+    #Recebe tamanho dos dados
     data_size = int.from_bytes(conn.recv(8), "big")
+    #Recebe dados em pedaços
     data = b""
     while len(data) < data_size:
         packet = conn.recv(4096)
         if not packet:
             break
         data += packet
+    # Desserializa os dados    
     subA, matrixB = pickle.loads(data)
     print(f"Received subA shape: {subA.shape}, matrixB shape: {matrixB.shape}")
+    # Medir tempo
     t0 = time.perf_counter()
     result = multiply(subA, matrixB)
     t1 = time.perf_counter()
     elapsed = t1 - t0
     print(f"    [server:{PORT}] Tempo multiplicação = {elapsed:.6f}s")
+    # Serialização do resultado e envio de volta ao cliente
     response = pickle.dumps(result)
     conn.sendall(len(response).to_bytes(8, "big"))
     conn.sendall(response)
